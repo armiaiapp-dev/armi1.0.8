2c91ea1fd25c3e71c58ae7d1f5fc5fe17159d2e4
armiaiapp-dev <armi.ai.app@gmail.com>
2025-08-30 20:18:11 -0600

    Remove padding from share screen preview

--- app/(share)/ShareScreen.tsx
+++ app/(share)/ShareScreen.tsx
@@ -576,8 +576,6 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    paddingTop: 20,
-    paddingBottom: 20,
   },
   cardWrapper: {
     transform: [{ scale: 0.3 }],
